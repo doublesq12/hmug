1ad08afaea1c3a55f1a2a9dc766d2b06113cce74
doublekou <951513186@qq.com>
2022-09-05 10:10:45 +0800

    创建项目

---
+++ pages/home/home.vue
@@ -0,0 +1,23 @@
+<template>
+  <view>
+    123
+    2132
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      }
+    },
+    methods: {
+      
+    }
+  }
+</script>
+
+<style>
+
+</style>
